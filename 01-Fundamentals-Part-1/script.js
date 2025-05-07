3d1789470e8c3d679061a8e20d574ca11e171a56
Jerry Rondon <jerryrondon@gmail.com>
2025-05-07 14:14:47 -0400

    Finalizando Values and Variables
    Fin del topico Values and Variables y resuelto el assignment
    https://jonas.io/assignments/values-and-variables.html

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,16 @@
 js = 'amazing';
-if (js === 'amazing') alert('JavaScript is amazing!!');
+
 
 console.log(33 + 25 + 62);
 
 let firstName = "Jerry";
 console.log(firstName);
+
+let lastName = "Rondon";
+console.log(lastName + " " + firstName);
+
+let country = "Venezuela";
+let continent = "America";
+let countryPopulation = 30000000;
+
+console.log("My country is: " + country + ", and is located in the continent of " + continent + ". Contains a population of " + countryPopulation + " of habitants.")
