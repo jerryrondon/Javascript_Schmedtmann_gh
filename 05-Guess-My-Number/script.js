a21b43e9cd41a59868c063e04e2bfda5a3065846
Jerry Rondon <jerryrondon@gmail.com>
2025-05-16 11:23:27 -0400

    Implementing the game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -19,19 +19,59 @@ console.log(document.querySelector('.guess').value);
 
 
 
+const secretNumber = Math.trunc((Math.random() * 20)) + 1;
+const initialScore = 5;
+let currentScore = initialScore;
+document.querySelector('.score').textContent = initialScore;
 
 
-function checkInputHandler() {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+console.log('Secret Number: ', secretNumber);
+// document.querySelector('.number').textContent = secretNumber;
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
-  }
+function lockGame() {
+  document.querySelector('.check').disabled = true;
+  document.querySelector('.again').style.background = '#fffb00'
 }
 
-// document.querySelector('.check').addEventListener('click', function () {
-//   console.log(document.querySelector('.guess').value);
-// })
+
+function gameOverMan() {
+  console.log('👽 Game Over Man!')
+  document.querySelector('.message').textContent = '👽 Game Over Man!';
+  document.querySelector('.number').textContent = secretNumber;
+}
+
+function updateScore() {
+  currentScore--;
+  document.querySelector('.score').textContent = currentScore;
+}
+
+function checkInputHandler() {
+  const guessInput = document.querySelector('.guess').value;
+  const guess = Number(guessInput);
+  console.log(guess, typeof guess);
+
+  if (currentScore > 0) {
+    if (!guessInput) {
+      document.querySelector('.message').textContent = '⛔ No number!';
+    } else if (guess < 1 || guess > 20) {
+      document.querySelector('.message').textContent = '✋ Between 1 and 20';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Congratulations ✨';
+      lockGame();
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      updateScore();
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      updateScore();
+    }
+  }
+  if (currentScore === 0) {
+    gameOverMan();
+    lockGame();
+  };
+}
+
+
 
 document.querySelector('.check').addEventListener('click', checkInputHandler);
