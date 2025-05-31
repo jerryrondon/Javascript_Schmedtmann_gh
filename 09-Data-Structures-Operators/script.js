1fb0b4426463775b65903945b439e7c3082088f5
Jerry Rondon <jerryrondon@gmail.com>
2025-05-30 20:50:57 -0400

    Destructuring Arrays

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -29,7 +29,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order: function (starterIndex, mainCourseIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainCourseIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -45,4 +47,51 @@ const restaurant = {
     },
   },
 };
+
+const arreglo = [2, 3, 4, 5];
+const a = arreglo[0];
+const b = arreglo[1];
+const c = arreglo[2];
+const d = arreglo[3];
+
+let [w, x, y, z] = arreglo;
+x = 10;
+console.log(w, x, y, z);
+console.log(arreglo);
+
+const [firstA, second] = restaurant.categories;
+console.log(firstA, second);
+
+const [firstB, , third] = restaurant.categories;
+console.log(firstB, third);
+
+
+// Intercambiar variables
+let [main, secondary] = restaurant.categories;
+console.log('Main: ', main, ' Secondary: ', secondary);
+
+[main, secondary] = [secondary, main];
+console.log('Main: ', main, ' Secondary: ', secondary);
+
+
+// Deconstructing function outputs
+console.log(restaurant.order(2, 0));
+const [starterCourse, mainCourse] = restaurant.order(2, 0);
+console.log('Starter: ', starterCourse);
+console.log('Main: ', mainCourse);
+
+
+// Arreglos anidados
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+
+// Default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
+
+const [p2 = 1, q2 = 1, r2 = 1] = [8, 9];
+console.log(p2, q2, r2);
+
 
