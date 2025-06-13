484ada1ae0d931006b310f406c91b3417be1b601
Jerry Rondon <jerryrondon@gmail.com>
2025-06-13 18:12:12 -0400

    Passing Arguments

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,7 +2,7 @@
 
 //////////////////////////////////////////
 // Default Parameters
-
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -28,3 +28,34 @@ createBooking('LH123', 2);
 
 // createBooking('LH123', , 300);
 createBooking('LH123', undefined, 300);
+*/
+
+
+
+
+//////////////////////////////////////////
+// Passing Arguments
+
+const flight = 'LH123';
+const passenger = {
+  passengerName: 'Jerry Rondon',
+  passport: 2734097324079035,
+}
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.passengerName = 'Mr. ' + passenger.passengerName;
+
+  if (passenger.passport === 2734097324079035) {
+    alert('Checked In!');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+checkIn(flight, passenger);
+console.log(flight);
+console.log(passenger);
+
+
+
