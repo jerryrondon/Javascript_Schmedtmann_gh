65de6574471287eccd45f2d6d0a8ecc75f329bf0
Jerry Rondon <jerryrondon@gmail.com>
2025-09-12 11:33:11 -0400

    Types of Events and Event Handlers

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -190,5 +190,24 @@ btnScrollTo.addEventListener('click', function (event) {
 
 
 
+///////////////////////////////////////
+// Types of Events and Event Handlers
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function () {
+  alert('addEventListener event');
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 5000);
+
+h1.onmouseenter = function () { alert('onmouseenter event'); };
+
+
+
+
+
 
 
