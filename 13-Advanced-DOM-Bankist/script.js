b6d9d036a73055a271ad22c84cbeb6e5623828cc
Jerry Rondon <jerryrondon@gmail.com>
2025-09-16 11:08:48 -0400

    Passing Arguments to Event Handlers - bind

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -108,28 +108,26 @@ tabsContainer.addEventListener('click', function (event) {
 // Menu fade animation
 const nav = document.querySelector('.nav');
 
-const handleHover = function (e, opacity) {
+const handleHover = function (e) {
+  console.log(this);
+
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if (el !== link) el.style.opacity = opacity;
+      if (el !== link) el.style.opacity = this;
     });
-    logo.style.opacity = opacity;
+    logo.style.opacity = this;
   }
 };
 
 
-nav.addEventListener('mouseover', function (e) {
-  handleHover(e, 0.5);
-});
+nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 
-nav.addEventListener('mouseout', function (e) {
-  handleHover(e, 1);
-});
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 
 
