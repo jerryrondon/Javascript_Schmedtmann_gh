b926448668a3382a783ff951c915c90d6d2049b6
Jerry Rondon <jerryrondon@gmail.com>
2025-09-16 09:52:02 -0400

    Passing Arguments to Event Handlers - part 1

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -105,6 +105,40 @@ tabsContainer.addEventListener('click', function (event) {
 
 
 
+// Menu fade animation
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+
+  }
+});
+
+
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 1;
+    });
+    logo.style.opacity = 1;
+
+  }
+});
+
+
+
 ///////////////////////////////////////
 ///////////////////////////////////////
 // Selecting, Creating, and Deleting Elements
