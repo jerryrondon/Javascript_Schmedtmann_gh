b1f25c85060e0757e223d86b2de44efd12cff8ac
Jerry Rondon <jerryrondon@gmail.com>
2025-09-19 12:02:27 -0400

    Constructor Functions and the new Operator

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // No hacer esto
+  this.calcAge = function () {
+    return 2025 - this.birthYear;
+  };
+};
+
+const jerry = new Person('Jerry', 1979);
+console.log(jerry);
+
+console.log(jerry instanceof Person);
+console.log(jerry.calcAge());
