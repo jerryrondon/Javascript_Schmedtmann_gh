84d03413acdc606c88dc5afdfcdce7d9cce64ecb
Jerry Rondon <jerryrondon@gmail.com>
2025-09-20 19:04:56 -0400

    Prototypal Inheritance on Built-In Objects

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -53,3 +53,42 @@ console.log(Person.prototype.hasOwnProperty('species'));
 ///////////////////////
 
 console.log('constructor:', Person.prototype.constructor === Person);
+
+
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+
+// jerry.__proto__ === Person.prototype
+console.log(jerry.__proto__);
+
+// Objeto
+console.log(jerry.__proto__.__proto__);
+
+// El padre de Object es null
+console.log(jerry.__proto__.__proto__.__proto__); // null
+
+
+console.dir(Person.prototype.constructor);
+
+const array = [9, 6, 4, 6, 4, 9, 76];
+console.log(array);
+
+console.log(array.__proto__); // constructor: Array
+console.log(array.__proto__ === Array.prototype); // true
+
+console.log(array.__proto__.__proto__); // constructor: Object
+
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(array.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+
+console.dir(x => x + 1);
+
