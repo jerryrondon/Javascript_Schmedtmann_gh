874b5906ba0ccfe9066cf94ad5d5f7b979bcbf08
Jerry Rondon <jerryrondon@gmail.com>
2025-09-21 18:14:08 -0400

    Seccion 14 - Challenge 1 - Fin

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -111,4 +111,28 @@ GOOD LUCK 😀
 */
 
 
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
 
+Car.prototype.accelerate = function () {
+  console.log(this.speed += 10);
+};
+
+Car.prototype.brake = function () {
+  console.log(this.speed -= 5);
+};
+
+
+const beamer = new Car('BMW', 120);
+const mercedes = new Car('Mercedes Benz', 95);
+
+console.log(beamer);
+console.log(mercedes);
+
+beamer.accelerate();
+beamer.brake();
+
+mercedes.accelerate();
+mercedes.brake();
